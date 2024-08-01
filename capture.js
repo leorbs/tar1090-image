8af7e4ed324b6fa7ce4c51785f1c6e793f21eae0
Leo <no@no.yes>
2024-08-01 21:18:47 +0200

    ad uncheck

--- capture.js
+++ capture.js
@@ -8,7 +8,31 @@ const puppeteer = require('puppeteer');
     const height = 1024;  // Desired height
     await page.setViewport({ width, height });
 
-    await page.goto('http://192.168.178.53/tar1090/?nowebgl&hideSideBar&hideButtons');
+    await page.goto('http://192.168.178.53/tar1090/?nowebgl&hideSideBar&hideButtons&mapContrast=-0.1&mapDim=-0.1&outlineWidth=4&outlineColor=ffffff&monochromeMarkers=000000&scale=1.7');
+
+
+    async function uncheckCheckbox(labelText) {
+        const checkboxLabel = await page.$x(`//label[text()='${labelText}']`);
+
+        if (checkboxLabel.length > 0) {
+            // Get the 'for' attribute value from the label
+            const checkboxId = await (await checkboxLabel[0].getProperty('htmlFor')).jsonValue();
+
+            // Create the selector for the checkbox using the 'for' attribute value
+            const checkboxSelector = `#${checkboxId}`;
+
+            // Uncheck the checkbox if it is checked
+            const isChecked = await page.$eval(checkboxSelector, checkbox => checkbox.checked);
+            if (isChecked) {
+                await page.click(checkboxSelector);
+            }
+        } else {
+            console.log(`Checkbox with the label '${labelText}' not found.`);
+        }
+    }
+
+    await uncheckCheckbox('actual range outline');
+    await uncheckCheckbox('DWD RADOLAN');
 
     // Function to take a screenshot
     const takeScreenshot = async () => {
