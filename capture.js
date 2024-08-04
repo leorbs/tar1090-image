988a860d2ef2fb1ad48311f516677f0ab805ca15
Leo <no@no.yes>
2024-08-04 16:18:51 +0200

    add restart. add black white refresh for epaper display

--- capture.js
+++ capture.js
@@ -1,46 +1,66 @@
 const puppeteer = require('puppeteer');
 
-const tarUrl = 'http://192.168.178.53/tar1090/?nowebgl&hideSideBar&hideButtons&outlineWidth=4&outlineColor=ffffff&monochromeMarkers=000000&scale=1.7&mapContrast=-0.1&mapDim=-0.1';
-const screenshotPath = '/media/screenshot.png';
+const tarUrl = 'http://192.168.178.53/tar1090/?nowebgl&hideSideBar&hideButtons&outlineWidth=3&outlineColor=ffffff&monochromeMarkers=000000&scale=1.7&mapContrast=-0.1&mapDim=-0.1';
+const screenshotPath = './media/screenshot.png';
+const width = 768;
+const height = 1024;
 
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    // Set the viewport size
-    const width = 768;  // Desired width
-    const height = 1024;  // Desired height
-    await page.setViewport({ width, height });
+const startBrowser = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    await page.goto(tarUrl);
+  // Set the viewport size
+  await page.setViewport({ width, height });
 
+  await page.goto(tarUrl);
 
-      // Function to uncheck a checkbox based on label text
-      const uncheckCheckbox = async (labelText) => {
-        await page.evaluate((text) => {
-          const label = Array.from(document.querySelectorAll('label'))
-            .find(label => label.textContent.trim() === text);
-          if (label) {
-            const checkbox = document.getElementById(label.htmlFor);
-            if (checkbox && checkbox.checked) {
-              checkbox.click();
-            }
-          }
-        }, labelText);
-      };
+  // Function to uncheck a checkbox based on label text
+  const uncheckCheckbox = async (labelText) => {
+    await page.evaluate((text) => {
+      const label = Array.from(document.querySelectorAll('label'))
+        .find(label => label.textContent.trim() === text);
+      if (label) {
+        const checkbox = document.getElementById(label.htmlFor);
+        if (checkbox && checkbox.checked) {
+          checkbox.click();
+        }
+      }
+    }, labelText);
+  };
 
-    await uncheckCheckbox('actual range outline');
-    await uncheckCheckbox('DWD RADOLAN');
+  await uncheckCheckbox('actual range outline');
+  await uncheckCheckbox('DWD RADOLAN');
 
-    // Function to take a screenshot
-    const takeScreenshot = async () => {
-        await page.screenshot({ path: screenshotPath });
-    };
+  // Function to take a screenshot
+  const takeScreenshot = async () => {
+    await page.screenshot({ path: screenshotPath });
+  };
 
-    // Take an initial screenshot
+  // Take an initial screenshot
+  await takeScreenshot();
+
+  // Take a screenshot every 10 seconds
+  const screenshotInterval = setInterval(async () => {
     await takeScreenshot();
+  }, 10000);
 
-    // Take a screenshot every 10 seconds
-    setInterval(async () => {
-        await takeScreenshot();
-    }, 10000);
-})();
+  // Function to stop the browser and clear interval
+  const stopBrowser = async () => {
+    clearInterval(screenshotInterval);
+    await browser.close();
+  };
+
+  return { browser, stopBrowser };
+};
+
+// Function to restart the browser every 12 hours
+const run = async () => {
+  let { stopBrowser } = await startBrowser();
+
+  setInterval(async () => {
+    await stopBrowser();
+    ({ stopBrowser } = await startBrowser());
+  }, 12 * 60 * 60 * 1000); // 12 hours in milliseconds
+};
+
+run();
